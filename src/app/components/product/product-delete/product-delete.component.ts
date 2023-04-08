bf0caa3add6d258c2776781145db5ea2362f21f5
irineub <122955363+irineub@users.noreply.github.com>
2023-04-08 16:24:44 -0400

    first-commit

---
+++ src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+  product!: Product;
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    if (id !== null) {
+      this.productService.readById(id).subscribe((product) => {
+        this.product = product;
+      });
+    }
+  }
+
+  deleteProduct(): void {
+    this.productService.delete(this.product).subscribe(() => {
+      this.productService.showMessage("Produto Apagado com Sucesso!");
+      this.router.navigate(["/products"]);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(["/products"]);
+    this.productService.showMessage("Operação Cancelada");
+  }
+}
---
+++ src/app/components/product/product.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar'
+import { Product } from './product.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseUrl = "http://localhost:3002/products"
+
+  constructor(private snackBar: MatSnackBar,
+    private http: HttpClient) { }
+
+  showMessage(msg:string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'Fechar', {
+      duration:3000,
+      panelClass:['class-success'],
+      horizontalPosition: "right",
+      verticalPosition: "top",
+    })
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product)
+  }
+
+  read(): Observable<Product[]>{
+  return this.http.get<Product[]>(this.baseUrl)
+}
+
+  readById(id :string):Observable<Product>{
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
+
+  delete(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.delete<Product>(url)
+  }
+
+
+
+}
